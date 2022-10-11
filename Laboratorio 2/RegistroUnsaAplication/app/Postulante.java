50e7fedc96ee75f97c2981b6ef9cc0b9102e4b41
Jimy Revilla <67451304+JRevillaT@users.noreply.github.com>
2022-10-10 21:14:31 -0500

    Se creo la clase Postulante, con su constructor, metodos get y set de cada atributo

---
+++ Laboratorio 2/RegistroUnsaAplication/app/Postulante.java
@@ -0,0 +1,60 @@
+public class Postulante {
+    private String apellidoPaterno, apellidoMaterno, nombres, fechaNacimiento, colegioProcedencia, carreraPostula;
+
+    public Postulante(String ap, String am, String n, String f, String col, String carr){
+        apellidoPaterno=ap;
+        apellidoMaterno=am;
+        nombres=n;
+        fechaNacimiento=f;
+        colegioProcedencia=col;
+        carreraPostula=carr;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getColegioProcedencia() {
+        return colegioProcedencia;
+    }
+
+    public void setColegioProcedencia(String colegioProcedencia) {
+        this.colegioProcedencia = colegioProcedencia;
+    }
+
+    public String getCarreraPostula() {
+        return carreraPostula;
+    }
+
+    public void setCarreraPostula(String carreraPostula) {
+        this.carreraPostula = carreraPostula;
+    }
+}
