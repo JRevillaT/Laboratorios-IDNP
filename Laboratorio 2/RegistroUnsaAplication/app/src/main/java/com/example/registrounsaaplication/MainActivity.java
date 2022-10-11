ec5f43e6535209a020cd6ef546bf6624af5c355e
Jimy Revilla <67451304+JRevillaT@users.noreply.github.com>
2022-10-11 16:58:08 -0500

    Concluida la parte de back-end. La aplicacion esta terminada

--- Laboratorio 2/RegistroUnsaAplication/app/src/main/java/com/example/registrounsaaplication/MainActivity.java
+++ Laboratorio 2/RegistroUnsaAplication/app/src/main/java/com/example/registrounsaaplication/MainActivity.java
@@ -37,19 +37,24 @@ public class MainActivity extends AppCompatActivity {
                 String apeMaterno= apeMat.getText().toString();
                 String nomAlum= nombres.getText().toString();
                 String fechNaci= fecNac.getText().toString();
-                String calPro= colegio.getText().toString();
+                String colPro= colegio.getText().toString();
                 String carPos= carrera.getText().toString();
 
-                listaPostulantes.add(new Postulante(apePaterno, apeMaterno, nomAlum, fechNaci, calPro, carPos));
+                if(!apePaterno.equals("") && !apeMaterno.equals("") && !nombres.equals("") && !fechNaci.equals("") && !colPro.equals("") && !carPos.equals("") ){
+                    listaPostulantes.add(new Postulante(apePaterno, apeMaterno, nomAlum, fechNaci, colPro, carPos));
 
-                Log.d("TAG", "Apellido Paterno: "+apePaterno);
-                Log.d("TAG", "Apellido Materno: "+apeMaterno);
-                Log.d("TAG", "Nombres: "+nomAlum);
-                Log.d("TAG", "Fecha de Nacimiento: "+fechNaci);
-                Log.d("TAG", "Colegio de Procedencia: "+calPro);
-                Log.d("TAG", "Carrera a la que Postula: "+carPos);
+                    Log.d("TAG", "\nApellido Paterno: "+apePaterno);
+                    Log.d("TAG", "Apellido Materno: "+apeMaterno);
+                    Log.d("TAG", "Nombres: "+nomAlum);
+                    Log.d("TAG", "Fecha de Nacimiento: "+fechNaci);
+                    Log.d("TAG", "Colegio de Procedencia: "+colPro);
+                    Log.d("TAG", "Carrera a la que Postula: "+carPos);
 
-                Toast.makeText(getApplicationContext(), "Postulante registrado exitosamente" , Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Postulante registrado exitosamente" , Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Error\nLlene todos los campos para realizar su registro" , Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -58,8 +63,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 for(Postulante p: listaPostulantes){
-                    Log.d("TAG","Apellidos y Nombres: "+ p.getApellidoPaterno() + p.getApellidoMaterno() + ", " + p.getNombres() + "\n Fecha de Nacimiento: " + p.getCarreraPostula() + "\nColegio de Procedencia: " + p.getColegioProcedencia() + "\nCarrera a la que postula: " + p.getCarreraPostula());
+                    Log.d("LISTADO","Apellidos y Nombres: "+ p.getApellidoPaterno() + p.getApellidoMaterno() + ", " + p.getNombres() + "\n Fecha de Nacimiento: " + p.getCarreraPostula() + "\nColegio de Procedencia: " + p.getColegioProcedencia() + "\nCarrera a la que postula: " + p.getCarreraPostula());
                 }
+                Toast.makeText(getApplicationContext(), "Listado Concluido.\nRevisa el LogCat de tu ordenador para visualizar el listado" , Toast.LENGTH_SHORT).show();
         }
     });
     }
